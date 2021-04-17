29ad8afde55c4100eda5fd2fc82e3bb55a716c2b
mohaddese136 <mohaddese136@gmail.com>
2021-04-18 00:40:18 +0430

    title with more btn

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -10,7 +10,64 @@ class Body extends StatelessWidget {
     // enables scrolling on small devices
     return SingleChildScrollView(
       child: Column(
-        children: [HeaderWithSearchBox(size: size)],
+        children: [
+          HeaderWithSearchBox(size: size),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Row(children: [
+              TitleWithCustomUnderline(
+                text: "محصولات پیشنهادی",
+              ),
+              Spacer(),
+              FlatButton(
+                  onPressed: () {},
+                  color: kPrimaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Text(
+                    "بیشتر",
+                    style: TextStyle(color: Colors.white),
+                  ))
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          )
+        ],
       ),
     );
   }

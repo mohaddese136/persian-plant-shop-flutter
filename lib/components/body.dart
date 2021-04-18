977fe2b5e3172c7d7a46ecd587ba40538c60637d
mohaddese136 <mohaddese136@gmail.com>
2021-04-18 19:41:16 +0430

    recomend plant card

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:testapp/components/recomend_plant_card.dart';
+import 'package:testapp/components/title_with_more_btn.dart';
 import 'package:testapp/constants.dart';
 
 class Body extends StatelessWidget {
@@ -12,147 +14,11 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: Row(children: [
-              TitleWithCustomUnderline(
-                text: "محصولات پیشنهادی",
-              ),
-              Spacer(),
-              FlatButton(
-                  onPressed: () {},
-                  color: kPrimaryColor,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "بیشتر",
-                    style: TextStyle(color: Colors.white),
-                  ))
-            ]),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
-    Key key,
-    this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24,
-      child: Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: kDefaultPadding / 4),
-            child: Text(
-              text,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
+          TitleWithMoreBtn(
+            title: "محصولات پیشنهادی",
+            press: () {},
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: EdgeInsets.only(right: kDefaultPadding / 4),
-              height: 7,
-              color: kPrimaryColor.withOpacity(0.2),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    Key key,
-    @required this.size,
-  }) : super(key: key);
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
-      // it will coveer only 20% of total height
-      height: size.height * 0.2,
-      child: Stack(
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-                left: kDefaultPadding,
-                right: kDefaultPadding,
-                bottom: 36 + kDefaultPadding),
-            height: size.height * 0.2 - 27,
-            decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36))),
-            child: Row(
-              children: [
-                Text(
-                  "سلام محدثه",
-                  style: Theme.of(context).textTheme.headline5.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                Spacer(),
-                Image.asset("assets/images/logo.png")
-              ],
-            ),
-          ),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-                height: 54,
-                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      )
-                    ]),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        onChanged: (value) {},
-                        decoration: InputDecoration(
-                          hintText: "جستجو",
-                          hintStyle:
-                              TextStyle(color: kPrimaryColor.withOpacity(0.5)),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          // suffix dos not work properly with TextField thats
-                          // why we use Row.
-                          // suffixIcon:
-                          //     SvgPicture.asset("assets/icons/search.svg")
-                        ),
-                      ),
-                    ),
-                    SvgPicture.asset("assets/icons/search.svg")
-                  ],
-                ),
-              ))
+          RecomendsPlants()
         ],
       ),
     );
